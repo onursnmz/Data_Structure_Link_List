a7fde7fa54c5524056334c4d47e46c0dc78fc769
onur sönmez <onursnmz@outlook.com>
2021-11-22 20:16:54 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Odev_DLinkList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(198, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 31);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "DELETE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(92, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "NUMBER";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(304, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(159, 276);
+            this.listBox1.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(198, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 31);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(89, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(527, 343);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev_DLinkList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class dugum
+        {
+            public string ad;
+            public int numara;
+            public dugum sonraki;
+            public dugum onceki;
+        }
+        dugum ilk = null;
+        public void print()
+        {
+            listBox1.Items.Clear();
+            dugum yeni = new dugum();
+            yeni = ilk;
+            while (yeni != null)
+            {
+                listBox1.Items.Add(yeni.numara);
+                yeni = yeni.sonraki;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dugum yeni = new dugum();
+            
+            yeni.numara = Convert.ToInt32(textBox2.Text);
+            if (ilk == null)
+            {
+                ilk = yeni;
+                ilk.sonraki = null;
+                ilk.onceki = null;
+            }
+            else
+            {
+                dugum iter,temp;
+                iter = ilk;
+                //Eğer başlangıç düğümünden küçükse yeni başlangıç düğümümüz, gelecek olan "yeni" düğüm olacaktır
+                if (yeni.numara < ilk.numara)
+                {
+                    temp = ilk;
+                    ilk = yeni;
+                    ilk.sonraki = temp;
+                    temp.onceki = ilk;
+                    ilk.onceki = null;
+                }//Linkli liste içerisinde girilen değeri yerine yerleştirme. ( büyük küçük kontrolleri yaparak )
+                else
+                {
+                    while (iter.sonraki != null && iter.sonraki.numara < yeni.numara)
+                    {
+                        iter = iter.sonraki;
+                    }
+                    if(iter.sonraki == null)
+                    {
+                        yeni.sonraki = iter.sonraki;
+                        iter.sonraki = yeni;
+                        yeni.onceki = iter;
+                    }
+                    else
+                    {
+                        yeni.sonraki = iter.sonraki;
+                        iter.sonraki.onceki = yeni;
+                        iter.sonraki = yeni;
+                        yeni.onceki = iter;
+                    }
+                }
+            }
+            print();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dugum iter;
+            iter = ilk;
+            dugum temp;
+            if (ilk == null)
+            {
+                label3.ForeColor = Color.Red;
+                label3.Text = "Linkli listede herhangi bir eleman yok!";
+            }
+            else
+            {
+                //Eğer sileceğimiz düğüm baştaysa, başlangıç düğümünü kaybetmemek adına temp adlı değişken
+                //oluşturup sileceğimiz düğümü o temp haline getirip öyle sildik. Daha sonrasında başlangıç düğümünü
+                //eski başlangıç'ın bir sonrasına attık(çünkü eski başlangıç silinmesi istendi)
+                if (ilk.numara == Convert.ToInt32(textBox2.Text))
+                {
+                    temp = ilk;
+                    ilk = ilk.sonraki;
+                    temp = null;
+                }
+                else
+                {
+                    while (iter.sonraki != null && iter.sonraki.numara != Convert.ToInt32(textBox2.Text))
+                    {
+                        iter = iter.sonraki;
+                    }
+                    if (iter.sonraki == null)
+                    {
+                        label3.ForeColor = Color.Red;
+                        label3.Text = "Girdiğiniz numara bulunamadı.";
+                    }
+                    else
+                    {
+                        temp = iter.sonraki;
+                        iter.sonraki = iter.sonraki.sonraki;
+                        temp = null;
+                    }
+                }
+            }
+            print();
+        }
+    }
+}
